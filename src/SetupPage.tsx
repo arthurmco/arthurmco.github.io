8572c933c634097edd6a1f190185ba15df916752
Arthur Mendes <arthurmco@gmail.com>
2025-09-05 21:30:45 -0300

    pages: add content into the setup page

--- src/SetupPage.tsx
+++ src/SetupPage.tsx
@@ -8,10 +8,41 @@ function SetupPage() {
                 <Link to="/">Voltar</Link>
             </section>
             <main>
-              <h2>Configuração</h2>
-              <hr />
-              TODO
-              
+                <h2>Configuração</h2>
+                <hr />
+                <section>
+                    <h3>PC padrão</h3>
+                    <ul>
+                        <li>AMD Ryzen 3950X</li>
+                        <li>48 GB de RAM</li>
+                        <li>SSD de 250 GB (<small>Windows 11</small>)</li>
+                        <li>SSD NVMe de 500 GB (<small>Linux, NixOS</small>)</li>
+                        <li>HD de 1TB (<small>compartilhado</small>)</li>
+                        <li>NVIDIA Geforce RTX 2070 SUPER</li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>Portátil</h3>
+                    <ul>
+                        <li>Intel Core i5-1334U</li>
+                        <li>16 GB de RAM</li>
+                        <li>SSD NVMe de 1 TB (<small>Linux, Fedora 42</small>)</li>
+                        <li>Intel Iris Xe</li>
+                    </ul>
+                </section>
+                <section>
+                    Em ambos os casos:
+                    <ul>
+                        <li>DE: <code>GNOME</code></li>
+                        <li>shell: <code>fish</code></li>
+                        <li>Editores/IDEs: <ul>
+                            <li><code>GNU emacs</code> no Linux</li>
+                            <li><code>vscode</code> no Windows</li>
+                            <li><code>neovim</code> como editor auxiliar</li>
+                        </ul></li>
+                        <li>Esquema de cores: alguma variação do <code>gruvbox</code></li>
+                    </ul>
+                </section>
             </main>
         </MainLayout>
     )
